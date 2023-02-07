f8318a62e8f9f63c9f818362378af4767cae5018
dfalfaro <diego.alfaro@tlgcohort.com>
2023-02-07 11:16:32 -0500

    added 5 day forecast

--- main.js
+++ main.js
@@ -7,22 +7,49 @@ const humidity = document.querySelector("#humidity");
 const windSpeed = document.querySelector("#windSpeed");
 const weatherIcon = document.querySelector("#weatherIcon");
 
-document.querySelector("form").addEventListener("submit", function(event) {
+document.querySelector("form").addEventListener("submit", function (event) {
   event.preventDefault();
   const city = document.querySelector("#city").value;
   const state = document.querySelector("#state").value;
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state},US&appid=6d9ae548620c2f3c7a8653d254edffc0`)
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${state},US&appid=6d9ae548620c2f3c7a8653d254edffc0`)
     .then(response => response.json())
     .then(data => {
-    weatherContainer.style.display = "block";
-    cityName.innerHTML = data.name;
-    temperature.innerHTML = `${Math.round((data.main.temp - 273.15) * 9/5 + 32)}°F`;
-    description.innerHTML = data.weather[0].description;
-    humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
-    windSpeed.innerHTML = `Wind Speed: ${data.wind.speed}m/s`;
-    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  })
-  .catch(error => {
-    console.error(error);
-  });
+
+      const forecastList = data.list;
+      const forecastData = {};
+
+      forecastList.forEach(forecast => {
+        const date = forecast.dt_txt.split(" ")[0];
+        if (!forecastData[date]) {
+          forecastData[date] = {
+            temperature: 0,
+            description: "",
+            icon: ""
+          };
+        }
+        forecastData[date].temperature += forecast.main.temp;
+        forecastData[date].description = forecast.weather[0].description;
+        forecastData[date].icon = forecast.weather[0].icon;
+      });
+      Object.keys(forecastData).forEach(date => {
+        forecastData[date].temperature /= 8;
+        forecastData[date].temperature = Math.round((forecastData[date].temperature - 273.15) * 9 / 5 + 32);
+      });
+      const forecastContainer = document.querySelector("#forecastContainer");
+      forecastContainer.innerHTML = "";
+      Object.keys(forecastData).forEach(date => {
+        const forecast = forecastData[date];
+        const forecastElement = document.createElement("div");
+        forecastElement.innerHTML = `
+          <h2>${date}</h2>
+          <p>Temperature: ${forecast.temperature}°F</p>
+          <p>Description: ${forecast.description}</p>
+          <img src="http://openweathermap.org/img/wn/${forecast.icon}@2x.png"/>
+        `;
+        forecastContainer.appendChild(forecastElement);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
